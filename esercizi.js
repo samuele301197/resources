842865151e47d8b98b153b088c538028c83d67d4
samuele301197 <parisisamu32@gmail.com>
2025-09-09 12:59:29 +0200

    adjust: correzione

--- esercizi.js
+++ esercizi.js
@@ -1,41 +1,35 @@
-async function getDashboardData(query) {
-  try {
-    const citta = await fetch(
-      `http://localhost:3333/destinations?search=${query}`
-    );
-    const meteo = await fetch(`http://localhost:3333/weathers?search=${query}`);
-    const aereoporto = await fetch(
-      `http://localhost:3333/airports?search=${query}`
-    );
-
-    const [cittaRes, meteoRes, aereoportoRes] = await Promise.all([
-      citta,
-      meteo,
-      aereoporto,
-    ]);
-
-    if (!cittaRes.ok || !meteoRes.ok || !aereoportoRes.ok) {
-      throw new Error("Qualcosa è andato male");
-    }
-
-    const [cittaData, meteoData, aereoportoData] = await Promise.all([
-      cittaRes.json(),
-      meteoRes.json(),
-      aereoportoRes.json(),
-    ]);
-
-    const result = {
-      city: cittaData[0].name,
-      country: cittaData[0].country,
-      temperature: meteoData[0].temperature,
-      weather: meteoData[0].weather_description,
-      airport: aereoportoData[0].name,
-    };
-    console.log(result);
-    return result;
-  } catch (err) {
-    console.error("Errore", err);
-  }
+async function fetchJson(url) {
+  const response = await fetch(url);
+  const obj = await response.json();
+  return obj;
 }
 
-getDashboardData("Dubai");
+async function getDashboardData(query) {
+  const citta = fetchJson(`http://localhost:3333/destinations?search=${query}`);
+  const meteo = fetchJson(`http://localhost:3333/weathers?search=${query}`);
+  const aereoporto = fetchJson(
+    `http://localhost:3333/airports?search=${query}`
+  );
+
+  const promises = [citta, meteo, aereoporto];
+  const [destinations, weather, airports] = await Promise.all(promises);
+
+  return {
+    city: destinations[0].name,
+    country: destinations[0].country,
+    temperature: weather[0].temperature,
+    weather: weather[0].weather_description,
+    airport: airports[0].name,
+  };
+}
+
+getDashboardData("london")
+  .then((data) => {
+    console.log("Dashboard data", data);
+    console.log(
+      `${data.city} is in ${data.country}. \n` +
+        `Today there are ${data.temperature} degrees and the weather is ${data.weather}. \n` +
+        `The main airport  is ${data.airport}. \n`
+    );
+  })
+  .catch((error) => console.error(error));
